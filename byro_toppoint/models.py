99d4907e5e2d288f15f3143e41eb5873d574e682
Moritz <MoritzFago@users.noreply.github.com>
2020-03-28 03:01:54 +0100

    Toppoint spezifische Felder ergänzt

--- byro_toppoint/models.py
+++ byro_toppoint/models.py
@@ -1,6 +1,7 @@
 from annoying.fields import AutoOneToOneField
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from byro.common.models.choices import Choices
 
 
 class ToppointProfile(models.Model):
@@ -15,3 +16,44 @@ class ToppointProfile(models.Model):
             null=False,
             blank=False,
     )
+    nachname = models.CharField(
+            verbose_name=_('Nachname'),
+            max_length=255,
+            null=False,
+            blank=False,
+    )
+    vermerk = models.TextField(
+            verbose_name=_('Vermerk'),
+            null=False,
+            blank=True,
+    )
+    beitragsklasse = models.IntegerField(
+            choices=Beitragsklasse.choices,
+            verbose_name=_("Beitragsklasse"),
+    )
+    
+    guardian = models.CharField(
+            verbose_name=_('Gesetzlicher Vertreter'),
+            max_length=255,
+            null=False,
+            blank=True,
+    )
+
+
+classe Beitragsklasse:
+    NORMAL = 0
+    STUDENT = 1
+    U18 = 2
+    PASSIV = 3
+    SOZIAL = 4
+    FAMILIE = 5
+
+    @classproperty
+    def choices(cls):
+        return (
+            (cls.NORNAL, _("Normal")),
+            (cls.STUDENT, _("Student/Schüler ab 18")),
+            (cls.U18, _("Jugend unter 18")),
+            (cls.PASSIV, _("Passiv")),
+            (cls.SOZIAL, _("Sozial")),
+            (cls.FAMILIE, _("Familie")
