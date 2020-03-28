77a332170e295f5a95b0ad0d1114194c62e172ed
Moritz <MoritzFago@users.noreply.github.com>
2020-03-28 02:21:07 +0100

    Model: vorname hinzugefügt

---
+++ byro_toppoint/models.py
@@ -0,0 +1,15 @@
+from annoying.fields import AutoOneToOne
+from django.db import models
+
+class ToppointProfile(models.Model):
+    member = AutoOneToOneField(
+            to='members.Member',
+            on_delete=models.CASCADE,
+            related_name='profile_toppoint',
+    )
+    vorname = models.CharField(
+            verbose_name=_('Vorname'),
+            max_length=255,
+            null=False,
+            blank=False,
+    )
